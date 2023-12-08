a9d17be0b48627cf18bda5998c478cce34ddefa9
dontyellwillcry <papaporo@proton.me>
2023-12-08 02:02:25 -0600

    added some code commnets to the server.js

--- server/server.js
+++ server/server.js
@@ -1,11 +1,8 @@
-// Require express - gives us a function
 const express = require('express');
 
-// Create an instance of express by calling the function returned above - gives us an object
 const app = express();
 const port = 5000;
 
-// express static file serving - public is the folder name
 app.use(express.static('server/public'));
 app.use(express.urlencoded({ extended: true }));
 
@@ -13,6 +10,7 @@ app.get("/", function (req, res) {
     res.sendFile(__dirname + "/public/index.html");
 });
 
+// Initializing an empty array to store todo items
 let todos = [];
 console.log(todos);
 
@@ -21,27 +19,36 @@ app.get('/getTodos', (req, res) => {
     res.send(updateTodoListHtml());
 });
 
+// Handling POST requests for adding new todos
 app.post('/addTodo', (req, res) => {
     const todoText = req.body.todoText;
+    // Creating a new todo object and pushing it to the 'todos' array
     todos.push({ id: todos.length + 1, text: todoText });
     res.send(updateTodoListHtml());
 });
 
+// Handling DELETE requests for deleting todos by ID
 app.delete('/deleteTodo/:id', (req, res) => {
     const todoId = parseInt(req.params.id);
+    // Filtering out the todo with the specified ID from the 'todos' array
     todos = todos.filter(todo => todo.id !== todoId);
     res.send(updateTodoListHtml());
 });
 
+// Handling PUT requests for toggling the completion status of todos by ID
 app.put('/toggleTodo/:id', (req, res) => {
     const todoId = parseInt(req.params.id);
+    // Finding the todo with the specified ID in the 'todos' array
     const todo = todos.find(todo => todo.id === todoId);
     
+    // Toggling the 'completed' property of the todo
     todo.completed = !todo.completed;
     
     res.send(updateTodoListHtml());
 });
 
+// Function to generate HTML for the todo list
+// Also handles the ternary operator for conditionally applying styles based on completion or not
 function updateTodoListHtml() {
     return `
         <ul style="list-style-type: none; padding: 0;">
@@ -57,7 +64,6 @@ function updateTodoListHtml() {
 
 
 
-// Start up our server
 app.listen(port, () => {
   console.log('listening on port', port);
 });
